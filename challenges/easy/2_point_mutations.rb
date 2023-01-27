b9e5584638a13f5cfc8a3aa0fddeb28c20b5e62f
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-01-27 15:55:28 -0500

    v2"

--- challenges/easy/2_point_mutations.rb
+++ challenges/easy/2_point_mutations.rb
@@ -1,72 +1,80 @@
 # rubocop:disable all
+
 =begin
-23 minutes PEDA
-6 minutes Coding
+20m PEDA
+15m Coding
 
 PROBLEM
-Description;
+Description: calculate the Hamming distance between two DNA strands
+
 The Hamming distance is only defined for sequences of equal length. 
 If you have two sequences of unequal length, 
-you should compute the Hamming distance over the shorter length.
-
-
-
-Input
-Output
-Rules
-
-
+  compute the Hamming distance over the shorter length.
 
 EXAMPLES / TEST CASES
-
-#1
 GAGCCTACTAACGGGAT
 CATCGTAATGACGGCCT
 ^ ^ ^  ^ ^    ^^
-
 The Hamming distance between these two DNA strands is 7.
 
+
+assert_equal 0, DNA.new('GGACTGA').hamming_distance('GGACTGA')
+# no diff => 0
+
+assert_equal 1, DNA.new('GGACG').hamming_distance('GGTCG')
+GGACG
+GGTCG
+  ^ 
+
+assert_equal 3, DNA.new('AAACTAGGGG').hamming_distance('AGGCTAGCGGTAGGAC')
+AAACTAGGGG
+AGGCTAGCGG #  TAGGAC
+ ^^    ^    # => 3
+
 DATA STRUCTURES
-Class: DNA
-  Attributes
-  Methods
-    #initialize
-      ('')
-      (String)
-    #hamming_distance
-      ('')
-      (String)
-      Return: Integer (0 or greater)
+class: DNA
+  #iniitialize(strand) -> dna_ob
+    returns a new DNA object
+    save strand for later use
+    no validation 
+    strand can be empty string
+  #hamming_distance(other_strand) -> distance
+    returns Integer, 0 or greater
 
 ALGORITHM
-Class: DNA
-  Attributes
-    Strand (String) -
-  Methods
-    #initialize
-      Input: String
-        string may be empty
-      Rules
-        assign Stran to argument
 
-    #hamming_distance
-      Input: String
-        string may be empty
-      Output: Integer
-        0 or greater
-      Rules
-        compare characters in strand and argument
-        compare characters at same index
-        compare until end of one string is reached
-        if characters are different += to distance
+#hamming_distance(other_strand)
+Input: String, other_strand; String, @strand
+Output: Integer
+Rules
+  no validation
+  either string can be empty
+  output is zero or greater
+  if sequences are uneven length compare only chars in shorter
 
-      Algo
-        distance = 0
-        i = 0
-        loop
-          break if strand[i].nil? || other_strand[i].nil?
-          distance += 1 if not equal
-          i +=1
+init hamming distance to 0
+find shorter string
+iterate through each char of shorter string
+compare char in short string and long string at each index
+  if not equal increase hamming distane by one
+
+
+pseudocode:
+init: short string
+    compare legths, assign
+init: long string
+    compare short_string object
+
+init array of short string chars
+map array with index
+    compare short char
+    long char at index
+    if not equal, 1
+    else 0
+return sum of array
+
+
+
 =end
 # rubocop:enable all
 class DNA
@@ -75,15 +83,14 @@ class DNA
   end
 
   def hamming_distance(other_strand)
-    distance = 0
-    i = 0
-
-    loop do
-      break if @strand[i].nil? || other_strand[i].nil?
-      distance += 1 unless @strand[i] == other_strand[i]
-      i += 1
-    end
-
-    distance
+    short, long = [@strand, other_strand].sort_by!(&:length)
+    
+    # v2
+    # short.chars.map.with_index do |char, i|
+    #   char == long[i] ? 0 : 1
+    # end.sum
+    
+    #v3
+    (0...short.length).to_a.count { |i| short[i] != long[i] }
   end
 end
