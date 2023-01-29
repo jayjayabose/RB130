c52662f27fc25ae9ca870d906cfa34946d9a07bc
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-01-29 16:22:55 -0500

    challenges 5 scrabble score v2

--- challenges/easy/5_scrabble_score.rb
+++ challenges/easy/5_scrabble_score.rb
@@ -1,31 +1,27 @@
 # rubocop:disable all
 =begin
-PEDA: 25min, C: 50 minutes (testing issues; solve quicky by stepping away and back to A;)
+PEDA: 30m, 23m Code
 
 PROBLEM
-Description
-Write a program that, given a word, computes the Scrabble score for that word.
+Write a program that, g
+iven a word, 
+computes the Scrabble score for that word.
 
 
-Input: String
-Output: Integer
+Input;
+Output:
 Rules:
- - given a word
- - each letter represents a tile
- - each tile has a value
- - sub vaues of all tiles in given word
- - case insenstive
-
 
 EXAMPLES / TEST CASES
 
-e.g.
-CABBAGE
+CABBAGE which has the following letters and point values:
+
 3 points for C
 1 point for each A (there are two)
 3 points for B (there are two)
 2 points for G
 1 point for E
+Thus, to compute the final total (14 points), we count:
 
 3 + 2*1 + 2*3 + 2 + 1
 => 3 + 2 + 6 + 3
@@ -33,71 +29,91 @@ CABBAGE
 => 14
 
 DATA STRUCTURES
+String
+Collection of some sort: letters and scores
 
+A, E, I, O, U, L, N, R, S, T	1
+D, G	2
+B, C, M, P	3
+F, H, V, W, Y	4
+K	5
+J, X	8
+Q, Z	10
+
+
+Integer
 
 class: Scrabble
-  attribute
-    @word 
-  methods
-    #initialize(String)
-      string may be '
-    #score() - returns score of argument supplied to constructor
-
+  #initialize(word) -> scrabble_obj 
+  #score() -> scrabble_score
+     
 
 ALGORITHM
+#initialize(word) -> scrabble_obj
+  Input: String
+  Output: new object
+  Rules:
+    word can be empty string
+    word can be: " \t\n"
+    word can be: nil
+    capital letters and/or lowercase letters
+    no errors raised
+    saves word for later use  
+      (change case?)
 
-#score
-init Hash: POINTS_PER_TILE
+#score() -> scrabble_score
+  Input: @word
+  Output: Integer
+  Rules:
+      scores are case insensitive
+      score is 0 or greater
+      each letter has a point value
+      word score is sum of point value of letters in the word
+    
 
-given String: word
+approach 1:
+  init: LOOKUP {'AEIOULNRST' => 1, ..}
+  init: KEYS array of keys in LOOKUP
 
-init: output = 0
-iterate over each letter
-  determine points per letter
-  add points to output
+  iterate through each char in word
+        init key: key in KEYS where char is in key
 
-return outout
+        
+approach 2
+  init: LETTER_VALUES = { /AEIOULNRST/ => 1}
+  init: Array: capitla letters in word
+      handle nil
 
-#process_input
-  handle nil
-    nil => return '' # empty string does not raise error
-  process string
-    remove non-letter chars
-    upcase
+  iterate through k, v pairs in LOOKUP: map
+    count chars in Array that match key * value
+
+  return sum of resulting array
+
 =end
 # rubocop:enable all
 class Scrabble
-  TILES_POINTS = {
-    ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']	=> 1,
-    ['D', 'G'] => 2,
-    ['B', 'C', 'M', 'P'] =>	3,
-    ['F', 'H', 'V', 'W', 'Y'] => 4,
-    ['K'] => 5,
-    ['J', 'X'] =>	8,
-    ['Q', 'Z'] =>	10
-  }.freeze
+  LETTER_VALUES = {
+    /[AEIOULNRST]/ =>	1,
+    /[DG]/ =>	2,
+    /[BCMP]/ => 3,
+    /[FHVWY]/ =>	4,
+    /K/ => 5,
+    /[JX]/ =>	8,
+    /[QZ]/ => 10
+  }
 
   def initialize(word)
-    @word = process_word(word)
-  end
-
-  def score
-    score = 0
-    @word.each_char do |letter|
-      tile_points_key = TILES_POINTS.keys.select { |k| k.include? letter }
-      score += TILES_POINTS[tile_points_key.flatten!]
-    end
-    score
+    @word = word || ''
   end
 
   def self.score(word)
     Scrabble.new(word).score
   end
 
-  private
-
-  def process_word(word)
-    return '' if word.nil?
-    word.gsub(/\s/, '').upcase
+  def score
+    letters = @word.upcase.chars
+    LETTER_VALUES.map do |regex, value|
+      letters.count { |letter| letter =~ regex } * value
+    end.sum
   end
 end
