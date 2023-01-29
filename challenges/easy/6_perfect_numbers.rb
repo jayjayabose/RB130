6be613842286c1ad186f090ae2977e3d0e4e3ef2
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-01-29 17:36:52 -0500

    challenges 6. perfect numbers v2

--- challenges/easy/6_perfect_numbers.rb
+++ challenges/easy/6_perfect_numbers.rb
@@ -1,90 +1,84 @@
 # rubocop:disable all
 =begin
-Total; 32m: PEDA: 20m; C: 12m (slight bump with case statement)
+PEDA 11min, Coding 19 min
 PROBLEM
-Description
+Write a program that can tell whether a number is 
+  perfect, abundant, or deficient.
 
-Write a program that can tell whether a number is perfect, abundant, or deficient.
-
-
-Input: Number
-Output; String
-Rules:
-  input 
-    is integer
-    is > 0
-  output (one of three classifications)
-    'dificient' | 'perfect' | 'abundant'
-  every valid input can be classified
-
-  Perfect 
-    Aliquot sum = original number.
-  Abundant 
-    Aliquot sum > original number.
-  Deficient
-    Aliquot <  original number.
-
-  Aliquot sum = sum of positive divisors of original number
-  positive divisors
-    numbers that can be evenly divided into number excluding number
-      e.g. positive divisors of 15 are 1, 3, and 5. 
-  
 EXAMPLES / TEST CASES
-6 is a perfect number since its 
-  divisors are 1, 2, and 3, and 
-  1 + 2 + 3 = 6.
-28 is a perfect number since its 
-  divisors are 1, 2, 4, 7, and 
-  14 and 1 + 2 + 4 + 7 + 14 = 28.
-15 is a deficient number since 
-  its divisors are 1, 3, and 5 and 
-  1 + 3 + 5 = 9 which is less than 15.
-24 is an abundant number since its 
-  divisors are 1, 2, 3, 4, 6, 8, and 
-  12 and 1 + 2 + 3 + 4 + 6 + 8 + 12 = 36 which is greater than 24.
+6 is a perfect number since its divisors are 
+  1, 2, and 3, and 1 + 2 + 3 = 6.
+28 is a perfect number since its divisors are 
+  1, 2, 4, 7, and 14 and 1 + 2 + 4 + 7 + 14 = 28.
+15 is a deficient number since its divisors are 
+  1, 3, and 5 and 1 + 3 + 5 = 9 which is less than 15.
+24 is an abundant number since its divisors are 
+  1, 2, 3, 4, 6, 8, and 12 and 1 + 2 + 3 + 4 + 6 + 8 + 12 = 36 which is greater than 24.
 
 Prime numbers 7, 13, etc. are always deficient since their only divisor is 1.
 
 DATA STRUCTURES
-class PerfectNumber
-  class method :: classify
-    Input: Integer
-    Output:
-      classification
-      Raise error if input < 1
+class: PerfectNumber
+  ::classify(number) -> classification
 
+Numbers
+ 
 ALGORITHM
-what about 1?
+  ::classify(number) -> classification
+  Input: number
+  Output: classification
+  Rules
+    number will be integer
+    raise error if number < 0
+    Aliquot sum
+      sum of divisors of number
+      exclude number
+      include 1
+    divisor
+      i % number == 0
+    return string 
+      'perfect': Aliquot sum  is equal to number.
+      'abundant' : Aliquot sum is greater than the number.
+      'deficient'  Aliquot sum less than the number.
 
-Given Number: num
-sum = 0
-# find divisors and add to sum
-  iterate from 1 .. num - 1 (can optimize this ) with index i
-    if n % i == 0, i is a divisor, add to sum
+  Approach
+    iterate 1 .. number - 1
+      if i is divisor, add to sum
 
-# compare to number to determine classification
-case 
-  sum < n then 'deficient'
-  sum . n then 'abundant'
-  abundant
+  Pseudocode
+    validate(number)
+    calculate aliquot_sum
+    init range: (1...number).to_a.select
+        blcok returns true for divisor
+    #sum
+
+    if sum > number
+      abundance
+    else if sum < number
+      deficient
+    else
+      perfect
 
 =end
 # rubocop:enable all
 class PerfectNumber
-  def self.classify(num)
-    validate_number(num)
+  def self.classify(number)
+    validate(number)
+    aliquot_sum = calculate_aliquot_sum(number)
 
-    sum = 0
-    1.upto(num - 1) { |i| sum += i if num % i == 0 }
-
-    return 'deficient' if sum < num
-    return 'abundant' if sum > num
-    'perfect'
+    case aliquot_sum <=> number
+    when 1 then 'abundant'
+    when -1 then 'deficient'
+    else 'perfect'
+    end
   end
 
-  def self.validate_number(num)
-    msg = 'Number must be Integer greater than zero'
-    raise StandardError, msg if num < 1
-    num
+  def self.validate(number)
+    msg = 'Argumeent must be a postive integer.'
+    raise StandardError, msg if number < 0
+  end
+
+  def self.calculate_aliquot_sum(number)
+    (1...number).to_a.select { |i| number % i == 0 }.sum
   end
 end
