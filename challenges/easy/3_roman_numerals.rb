a17eb7bd42e72e3d40f5c3389c962a2489b5d8a0
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-01-29 15:13:22 -0500

    challenges 3 roman numberals v2

--- challenges/easy/3_roman_numerals.rb
+++ challenges/easy/3_roman_numerals.rb
@@ -1,129 +1,110 @@
 # rubocop:disable all
 =begin
-PEDA: 1hr + 10 minutes
-Coding: 20 minutes
+PEDA: 45m; C: 29
 
+pickup notes
+ - pseudo code for #convert is close
 PROBLEM
-Description: 
-Write some code that converts modern decimal numbers into their Roman number equivalents.
 
 EXAMPLES / TEST CASES
-
 1990 is MCMXC:
-M+CM+XC:
   1000=M
   900=CM
   90=XC
 
+2008 is MMVIII:  
+2000=MM
+8=VIII
 
-2008 is written as MMVIII
-MM+VIII
-  2000=MM
-  8=VIII
+DATA STRUCTURES
+Integer
+String
+Hash
 
-smaller number left of bigger number indiates subtraction: IV = 4
-1-3: I, II, III, 
-4-5: IV, V
-6-8: VI, VII, VIII, 
-9-10: IX, X
-
-11-13: XI, XII, XIII, 
-13-15: XIV, XV
-
-DATA STRUCURES
 class: RomanNumeral
-  attribute
-    number
-  methods
-    #constructor()
-      takes a number 1-3000
-    #to_roman
-      return: roman number equivalent
-
-ALGORITHM
-
-given number: e.g 1024
-1. separate number to "place values":
-  e.g. 
-  4
-  20
-  1000
-  => [1,0,2,4]
-
-  steps
-    convert to string
-    convert to array of characters
+  #new(num) -> roman_numeral_obj
+    saves Integer num for later use
+    raise no errors
+  #to_roman() -> roman_numeral
+    returns a String representing a roman numeral
 
 
-2 convert each digit to roman form  
-  e.g. 
-  4 => IV
-  20 = XX
-  1000 = M
-  => ['M', nil, 'XX', 'IV']
+ALGORITHMS
 
-  steps
-    given array,  
-    reverse array
-    transform array
-      iterate through each element, with index
-        convert_digit (element, index)
-    reverse array
+#to_roman()
+  Input: @number
+  Output: String 
+  pseudocode
+  separate @number array of Integers # => e.g. '1990'
+    @number -> string
+    -> chars
+    -> to_i
+    # =>e.g. [1, 9, 9, 0]
+ 
+  map!.with index |digit, i|
+    init position: length - i
+    convert_digit(digit, position) 
+  # join and output trainsformed array
 
-3 assemble output
-=> M+XX+IV
 
-convert_digit(digit, index)
-====
-convert digit to integer
-unit = char_lookup_hashes[i][:unit]
+#convert(digit, place value) -> roman_chars
+input: String, Integer 
+output: String
+rules
+  digit is 0-9
+  place value is an Integer 1-4
+    4; thousands
+    1: ones
+pseudocode  
+  init: placevalue = PLACE_VALUES[digit]
+  init one, five, ten = placevalue['one'], placevalue['five'], placevalue['ten'],
+  reassign: digit.to_i
+  
+  case digit
+    0..3; one * digit
+    4; one + five
+    5: five
+    6..8: five + one * digit
+    9: one + ten
 
-ones = {unit: 'I', midpoint: 'V', next_unit: 'X'}
-tens = {unit: 'X', midpoint: 'L', next_unit: 'C'}
-hundreds = {unit: 'C', midpoint: 'D', next_unit: 'M'}
-thousands = {unit: 'M', midpoint: 'V̅', next_unit: 'X̅'}
-char_lookup_hashes = [ones, tens, hundreds, thousands]
-
-case (1..3) then: unit * digit
-case 4 then unit + midpoint
-case 5 then midpoint
-case 5..8 then midpoint + unit * digit
-case 9 then unit + next_unit
 =end
 # rubocop:enable all
-
 class RomanNumeral
-  ONES = { unit: 'I', mid: 'V', next_unit: 'X' }.freeze
-  TENS = { unit: 'X', mid: 'L', next_unit: 'C' }.freeze
-  HUNDREDS = { unit: 'C', mid: 'D', next_unit: 'M' }.freeze
-  THOUSANDS = { unit: 'M', mid: 'V̅', next_unit: 'X̅' }.freeze
-  CHAR_LOOKUP = [ONES, TENS, HUNDREDS, THOUSANDS].freeze
+  ONES = { 'one' => 'I', 'five' => 'V', 'ten' => 'X' }
+  TENS = { 'one' => 'X', 'five' => 'L', 'ten' => 'C' }
+  HUNDREDS = { 'one' => 'C', 'five' => 'D', 'ten' => 'M' }
+  THOUSANDS = { 'one' => 'M', 'five' => 'V̅', 'ten' => 'X̅' }
+  POSITION_PLACE_VALUES = {
+    4 => THOUSANDS,
+    3 => HUNDREDS,
+    2 => TENS,
+    1 => ONES
+  }
 
   def initialize(number)
     @number = number
   end
 
   def to_roman
-    digits = @number.to_s.chars
-    digits.reverse!.map!.with_index do |digit, index|
-      convert_digit(digit, index)
+    digits = @number.to_s.chars.map!(&:to_i)
+    digits.map!.with_index do |digit, i|
+      position = digits.length - i
+      convert_digit(digit, position)
     end
-    digits.reverse!.join
+    digits.join
   end
 
   private
 
-  def convert_digit(digit, index)
-    unit = CHAR_LOOKUP[index][:unit]
-    mid = CHAR_LOOKUP[index][:mid]
-    next_unit = CHAR_LOOKUP[index][:next_unit]
-
-    case digit.to_i
-    when (1..3) then unit * digit
-    when 4 then unit + mid
-    when 5 then mid
-    when (6..8) then mid + unit * (digit - 5)
-    when 9 then unit + next_unit
+  def convert_digit(digit, position)
+    place_value = POSITION_PLACE_VALUES[position]
+    one, five, ten = place_value.values
+    case digit
+    when 0..3 then one * digit
+    when 4 then one + five
+    when 5 then five
+    when 6..8 then five + one * (digit - 5)
+    when 9 then one + ten
     end
   end
 end
